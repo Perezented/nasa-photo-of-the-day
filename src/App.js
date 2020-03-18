4014b10185f22309105f2ac1e4db3536dbdc604c
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-18 14:34:37 -0500

    I've added axios to the Today.js. I ama able to console.log todays information. Just struggling a little to get it displayed.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import Today from "./components/Today";
 
 function App() {
     // https://api.nasa.gov/planetary/apod?api_key=bOSofFFDfPE8KYcweoQPKvuVEpFcAxtTn2JEHqPh
     return (
         <div className="App">
             <h1>This is NASA's photo of the day!</h1>
-            <p>This information is retrieved from the nasa.gov api.</p>
+            <Today />
         </div>
     );
 }
--- src/components/Today.js
+++ src/components/Today.js
@@ -1,2 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+export default function Today() {
+    const [info, updatedInfo] = useState([]);
+    useEffect(() => {
+        axios
+            .get(
+                "https://api.nasa.gov/planetary/apod?api_key=bOSofFFDfPE8KYcweoQPKvuVEpFcAxtTn2JEHqPh"
+            )
+            .then(result => {
+                console.log(result.data);
+                updatedInfo(result.data);
+            })
+            .catch(error => console.log(error));
+    }, []);
+    return (
+        <section>
+            <h2>title</h2>
+            <h5>Date</h5>
+            <p>{updatedInfo}</p>
+        </section>
+    );
+}
