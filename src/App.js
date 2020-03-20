680e7939b4429a0eff9b588cc617f8b210a80c7f
Michael Perez <mperez6473@gmail.com>
2020-03-19 20:57:32 -0500

    did regular install and styled-components already, doing axios right now. Added the main heading. Needing to get information from the server to display live info.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 import styled from 'styled-components';
-
+// https://api.nasa.gov/planetary/apod?api_key=bOSofFFDfPE8KYcweoQPKvuVEpFcAxtTn2JEHqPh
 function App() {
     return (
         <div className="App">
-            <p>test</p>
+
         </div>
     );
 }
